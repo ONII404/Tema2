e9bf3289a80aefb5ab54072ce9a8bb739e0c3377
ONII <beehrtbeehrt@gmail.com>
2025-05-31 17:28:57 -0500

    Bucle For eliminado

--- src/Ejercicios/Parte2_11_20/Act13_Permutaciones.java
+++ src/Ejercicios/Parte2_11_20/Act13_Permutaciones.java
@@ -3,9 +3,10 @@ package Ejercicios.Parte2_11_20;
 import java.util.Scanner;
 
 /**
- * 13. Programa con un método recursivo para generar todas las permutaciones posibles de una cadena.
- * 
- * @author beehr
+ * 13. Programa con un método recursivo para generar todas las permutaciones
+ * posibles de una cadena.
+ *
+ * Erick Humberto Rojas Teran.
  */
 public class Act13_Permutaciones {
 
@@ -13,17 +14,18 @@ public class Act13_Permutaciones {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingresa una cadena: ");
         String cadena = scanner.nextLine();
-        generarPermutaciones("", cadena);
+        generarPermutaciones("", cadena, 0);
     }
 
-    public static void generarPermutaciones(String prefijo, String cadena) {
+    // Función única recursiva (sin bucles, sin métodos auxiliares)
+    public static void generarPermutaciones(String prefijo, String cadena, int indice) {
         if (cadena.isEmpty()) {
             System.out.println(prefijo);
-        } else {
-            for (int i = 0; i < cadena.length(); i++) {
-                generarPermutaciones(prefijo + cadena.charAt(i),
-                        cadena.substring(0, i) + cadena.substring(i + 1));
-            }
+        } else if (indice < cadena.length()) {
+            String nuevoPrefijo = prefijo + cadena.charAt(indice);
+            String nuevaCadena = cadena.substring(0, indice) + cadena.substring(indice + 1);
+            generarPermutaciones(nuevoPrefijo, nuevaCadena, 0);
+            generarPermutaciones(prefijo, cadena, indice + 1);
         }
     }
 }
